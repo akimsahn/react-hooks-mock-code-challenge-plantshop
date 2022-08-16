345b0d2f68080fd01bcef3e11f5bab050368e4ce
Allison Kim <akeem159@yahoo.com>
2022-08-16 15:48:05 -0400

    commit tree 3988cc021ccc59950f195d5338802da90c5a7d4e

--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -1,14 +1,35 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
 
 function PlantPage() {
+  const [plants, setPlants] = useState([])
+  const [search, setSearch] = useState("")
+
+  useEffect(() => {
+    fetch('http://localhost:6001/plants')
+    .then(res => res.json())
+    .then(plantData => setPlants(plantData))
+  },[]);
+
+  function handleNewPlant(newPlant) {
+    setPlants([...plants, newPlant])
+  }
+
+  function handleSearch(searchText) {
+    setSearch(searchText)
+  }
+
+  const plantsToDisplay = plants.filter(plant => (
+    search === "" ? true : plant.name.toUpperCase().indexOf(search.toUpperCase()) > -1
+  ))
+
   return (
     <main>
-      <NewPlantForm />
-      <Search />
-      <PlantList />
+      <NewPlantForm onSubmitForm={handleNewPlant} />
+      <Search searchText={search} onSearch={handleSearch} />
+      <PlantList plants={plantsToDisplay} />
     </main>
   );
 }
